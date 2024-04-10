2926de7f54ac071dd6c1aa5ec67e0084387063a1
Gerlei Gandra <gerleigandra@outlook.com>
2024-04-10 12:58:41 -0300

    Subindo arquivos

---
+++ src/components/paragrafo/paragrafo.js
@@ -0,0 +1,10 @@
+const paragrafo = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem magni eos ducimus, adipisci dicta quos nisi nemo delectus alias consequatur quaerat vero veniam dolores, autem recusandae cumque, non unde sapiente?'
+
+const color = 'red'
+
+const Paragrafo = () => {
+   return <p style={{color: color}}>{paragrafo.toUpperCase()}</p>
+   
+}
+
+export default Paragrafo
